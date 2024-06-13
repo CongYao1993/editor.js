6421b372bc3cc0bf25f1c0a73cfc46d7356c8e38
yaocong01 <cong1207@qq.com>
2024-06-13 23:16:20 +0800

    修改细节

--- src/component/EditArea.tsx
+++ src/component/EditArea.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { Document } from './Document';
 import { Document as DocumentModel } from '../model/Document';
+import './style.css';
+
+const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
 
 export function EditArea(props: { data: any }) {
     const document = DocumentModel.create(props.data);
     return (
-        <div>
+        <div
+            className='editArea'
+            contentEditable="true"
+            onKeyDown={onKeyDown}
+        >
             <Document document = {document} />
         </div>
     )
